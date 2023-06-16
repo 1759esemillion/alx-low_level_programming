a2f6d2fab4328da3bdba7094aa9d1cee28fd0c9f
1759esemillion <esemillion@gmail.com>
2023-06-16 16:54:55 -0700

    12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,7 +5,7 @@
 */
 int main(void)
 {
-        int i, j;
+	int i, j;
 
 	for (i = 0; i < 100; i++)
 	{
@@ -13,14 +13,14 @@ int main(void)
 		{
 			if (i < j)
 			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
+				putchar((i / 10) + 0);
+				putchar((i % 10) + 0);
 				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
+				putchar((j / 10) + 0);
+				putchar((j % 10) + 0);
 				if (i != 98 || j != 99)
 				{
-					putchar(' ');
+					putchar(' , ');
 					putchar(' ');
 				}
 			}
