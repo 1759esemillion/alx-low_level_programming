f00ed50e950c0b337f0750fdd83a0d9d2bbe6a33
1759esemillion <esemillion@gmail.com>
2023-06-15 18:01:45 -0700

    h

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,11 @@
  * main - Entry point
  *
  * return: Always 1 (Success)
+ *
+ * Description: This function is the entry point of the program. The program should return 1 for successful execution
 */
 int main(void)
 {
-        write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-        return (1);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }   
