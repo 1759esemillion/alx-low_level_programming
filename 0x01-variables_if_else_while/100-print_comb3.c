7536e45699b6a3265c86c6911287fef8173c86ae
1759esemillion <esemillion@gmail.com>
2023-06-16 17:23:13 -0700

    All

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	int n, m;
 
-	for (n =48; n <= 56; n++)
+	for (n = 48; n <= 56; n++)
 	{
 		for (n = 49; m <= 57; m++)
 		{
@@ -17,7 +17,7 @@ int main(void)
 				putchar(m);
 				if (n != 56 || m != 57)
 				{
-					putchar(' ');
+					putchar(',');
 					putchar(' ');
 				}
 			}
@@ -25,4 +25,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}	
+}
