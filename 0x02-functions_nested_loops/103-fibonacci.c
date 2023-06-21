756cedef373d6992f5d5df34311badcbaf806ba8
1759esemillion <esemillion@gmail.com>
2023-06-21 07:20:39 -0700

    2

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,24 +8,25 @@
 
 int main(void)
 {
-	int x;
-	unsigned long int y, z, next, sum;
+        int x;
+        unsigned long int y, z, next, sum;
 
-	y = 1;
-	z = 2;
-	sum = 0;
+        y = 1;
+        z = 2;
+        sum= 0;
 
-	for (x = 1, x <= 33; x++)
-	{
-		if (y < 4000000 && (y % 2) == 0)
-		{
-			sum = sum + y;
-		}
-		next = y + z;
-		y = Z;
-		z = next;
-	}
+        for (x = 1; x <= 33; x++)
+        {
+                if (y < 4000000 && (y % 2) == 0)
+                {
+                        sum = sum + y;
+                }
+                next = y + z;
+                y = z;
+                z = next;
+        }
 
-	printf("%lu\n", sum);
-	return (0);
+        printf("%lu\n", sum);
+        return (0);
 }
+
