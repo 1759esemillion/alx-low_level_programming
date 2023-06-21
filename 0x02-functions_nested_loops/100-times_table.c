afb3c37acd7315ecd9c765c7b0f64e9da1525bb3
1759esemillion <esemillion@gmail.com>
2023-06-21 05:07:39 -0700

    1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,24 +18,25 @@ void print_times_table(int n)
 			{
 				z = y * x;
 
-				if (y == 0)
-				{
-					_putchar(z + '0');
-				} else if (z < 10 && y != 0)
-				{
+				if (z < 10)
+				{	
+					if (y != 0)
+					
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
+				}
+				_putchar('0' + z)
 				
-				} else if (z >= 10 && z < 100)
+				else if (z < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar((z / 100) + '0');
 					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
-				} else if (z >= 100)
+				} else
 				{
 					_putchar(',');
 					_putchar(' ');
