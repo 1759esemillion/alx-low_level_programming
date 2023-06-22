f5ce3fc18cbc578744da7cf0b046539e9cc89b73
1759esemillion <esemillion@gmail.com>
2023-06-21 19:55:55 -0700

    debug

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - code that is causing the output to go into an infinite loop
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming : (\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar(i)
+	/*}*
